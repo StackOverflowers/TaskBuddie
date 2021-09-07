d56bb61addd531552fce2ed1718eee8ff7ba35ab
Diana Chacon <dchacon871@unab.edu.co>
2021-09-07 09:19:53 -0500

    Route list user admin

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListUserComponent } from './admin/list-user/list-user.component';
 import { LoginComponent } from './home/login/login.component';
 import { RegisterComponent } from './home/register/register.component';
 import { ListTaskComponent } from './task/list-task/list-task.component';
@@ -8,12 +9,12 @@ const routes: Routes = [
   { path: '', component: LoginComponent, pathMatch: 'full' },
   { path: 'registerUser', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
-  
- 
+  { path: 'listTask', component: ListTaskComponent },
+  { path: 'listUser', component: ListUserComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
