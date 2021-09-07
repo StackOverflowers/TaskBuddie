4534cd83497b3127d867baad119ecb7ad3a11068
Diana Chacon <dchacon871@unab.edu.co>
2021-09-07 10:19:22 -0500

    Route list board

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListBoardComponent } from './board/list-board/list-board.component';
 import { LoginComponent } from './home/login/login.component';
 import { RegisterComponent } from './home/register/register.component';
 import { ListTaskComponent } from './task/list-task/list-task.component';
@@ -8,12 +9,12 @@ const routes: Routes = [
   { path: '', component: LoginComponent, pathMatch: 'full' },
   { path: 'registerUser', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
-  
- 
+  { path: 'listBoard', component: ListBoardComponent },
+  { path: 'listTask', component: ListTaskComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
