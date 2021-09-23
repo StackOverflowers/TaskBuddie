77ddae33281e8070f6fb4aa1031dc562753e17b1
Camilo Rodriguez <camilo.rod2412@gmail.com>
2021-09-23 16:52:53 -0500

    changes env

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,8 @@ const { dbConnection } = require("./db/db");
 const Role = require("./routes/role");
 const Board = require("./routes/board");
 const User = require("./routes/user");
-const Task = require('../backend/routes/task');
+const Task = require('./routes/task');
+const path = require("path");
 
 require("dotenv").config();
 
@@ -16,9 +17,12 @@ app.use("/api/role", Role);
 app.use("/api/user", User);
 app.use("/api/board", Board);
 app.use('/api/task',Task);
-app.use("/uploads", express.static("uploads"))
+app.use("/uploads", express.static("uploads"));
+app.use(express.static("public"));
 
-
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "public/index.html"));
+});
 
 app.listen(process.env.PORT, () =>
   console.log("Backend server running on port: " + process.env.PORT)
